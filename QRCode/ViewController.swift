4e7cddc2d0ef0f7387418d13bfb54dce439e3d0f
Chutintron Suwannaklom <chutintronsuwannaklom@MacBook-Pro-khxng-Chutintron.local>
2017-10-17 22:16:58 +0700

    commit master

--- QRCode/ViewController.swift
+++ QRCode/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    @IBOutlet weak var square: UIImageView!
+    var video = AVCaptureVideoPreviewLayer()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        /*let session = AVCaptureSession()
+        let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
+        do{
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            session.addInput(input)
+
+        }
+        catch{
+            print("ERROR")
+        }
+        
+        let output = AVCaptureMetadataOutput()
+        session.addOutput(output)
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        output.metadataObjectTypes = [.qr,.ean8, .ean13, .pdf417]
+        
+        video = AVCaptureVideoPreviewLayer(session: session)
+        video.frame = view.layer.bounds
+        view.layer.addSublayer(video)
+        
+        self.view.bringSubview(toFront: square)
+        
+        session.startRunning()*/
+    }
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        if metadataObjects != nil && metadataObjects.count != 0{
+            if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject{
+                if object.type == AVMetadataObject.ObjectType.qr{
+                    let alert = UIAlertController(title:"QR Code", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title:"Retake", style: .default, handler: (nil)))
+                    alert.addAction(UIAlertAction(title:"Copy", style: .default, handler: { (nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                    present(alert, animated: true, completion: nil)
+                }else{
+                    let alert = UIAlertController(title:"Other", message: object.stringValue, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title:"Retake", style: .default, handler: (nil)))
+                    alert.addAction(UIAlertAction(title:"Copy", style: .default, handler: { (nil) in
+                        UIPasteboard.general.string = object.stringValue
+                    }))
+                    present(alert, animated: true, completion: nil)
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
