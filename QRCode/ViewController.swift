ccbd7a2656b71ab939084929c2e21b30715c611c
Chutintron Suwannaklom <chutintronsuwannaklom@MacBook-Pro-khxng-Chutintron.local>
2017-10-17 22:52:57 +0700

    add button scan

--- QRCode/ViewController.swift
+++ QRCode/ViewController.swift
@@ -12,16 +12,36 @@ import AVFoundation
 class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
     
     @IBOutlet weak var square: UIImageView!
+    @IBOutlet weak var btnOpenCamera: UIButton!
     var video = AVCaptureVideoPreviewLayer()
+    var session = AVCaptureSession()
+    private var toggleCamera:Bool = true
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        /*let session = AVCaptureSession()
+        
+        
+        self.view.bringSubview(toFront: square)
+        
+        
+    }
+    
+    @IBAction func OpenCamera(_ sender: Any) {
+        toggleCamera = !toggleCamera;
+        if(toggleCamera){
+            if let inputs = session.inputs as? [AVCaptureDeviceInput] {
+                for input in inputs {
+                    session.removeInput(input)
+                }
+            }
+            return
+        }
+        session = AVCaptureSession()
         let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
         do{
             let input = try AVCaptureDeviceInput(device: captureDevice!)
             session.addInput(input)
-
+            
         }
         catch{
             print("ERROR")
@@ -36,10 +56,12 @@ class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
         video.frame = view.layer.bounds
         view.layer.addSublayer(video)
         
-        self.view.bringSubview(toFront: square)
         
-        session.startRunning()*/
+        self.view.bringSubview(toFront: btnOpenCamera)
+        session.startRunning()
     }
+    
+    
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         if metadataObjects != nil && metadataObjects.count != 0{
             if let object = metadataObjects[0] as? AVMetadataMachineReadableCodeObject{
